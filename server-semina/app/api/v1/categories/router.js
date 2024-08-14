ad8e793e7ab3068f58b9792b879a884e7053391a
Jibalfa <najibalfarizi5@gmail.com>
2024-08-14 10:39:35 +0700

    fix categories router

--- server-semina/app/api/v1/categories/router.js
+++ server-semina/app/api/v1/categories/router.js
@@ -8,4 +8,5 @@ router.get("/categories/:id", find);
 router.post("/categories", create);
 router.put("/categories/:id", update);
 router.delete("/categories/:id", destroy)
+
 module.exports = router;
