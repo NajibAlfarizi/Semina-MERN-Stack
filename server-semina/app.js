71f2fa52c974662d2de75cfe9da1e070eb923552
Jibalfa <najibalfarizi5@gmail.com>
2024-08-14 10:41:43 +0700

    implement image & talents api

--- server-semina/app.js
+++ server-semina/app.js
@@ -7,6 +7,9 @@ var app = express();
 
 // router
 const categoriesRouter = require("./app/api/v1/categories/router");
+const imagesRouter = require("./app/api/v1/images/router");
+const talentRouter = require('./app/api/v1/talents/router');
+
 const v1 = "/api/v1/cms";
 
 // middleware
@@ -26,6 +29,8 @@ app.get("/", (req, res) => {
 });
 
 app.use(v1, categoriesRouter);
+app.use(v1, imagesRouter);
+app.use(v1, talentRouter);
 
 app.use(notFoundMiddleware)
 app.use(handleErrorMiddleware)
